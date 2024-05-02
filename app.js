42c2814149c770d3ecc5e2f0a693d59987d672d7
azamjonoff <abdullohazamjonov57gmail.com>
2024-05-02 06:47:51 +0500

    2 matters ready

---
+++ app.js
@@ -0,0 +1,70 @@
+// massiv - []
+
+// const names = [
+//   "Nurmuhammad",
+//   "Firdavs",
+//   "Rahmonjon",
+//   "Dilmurodjon",
+//   "Farxodjon💔",
+//   "Eldorbek🍎",
+//   "Abdurahmon",
+//   "Abdulloh 2x",
+// ];
+
+// let counterjon = 0;
+// let counterbek = 0;
+
+// for (let i = 0; i < names.length; i++) {
+//   if (names[i].includes(`jon`)) {
+//     counterjon++;
+//   } else if (names[i].includes(`bek`)) {
+//     counterbek++;
+//   }
+// }
+
+// console.log(counterjon);
+// console.log(counterbek);
+
+// UYGA VAZIFA
+
+// 1)
+
+// function getInitialOdds(n) {
+//   let oddNumbers = [];
+//   let k = 1;
+
+//   for (i = 1; i <= n; i++) {
+//     if (i == 1) {
+//       oddNumbers.push(i);
+//     } else {
+//       oddNumbers.push((k += 2));
+//     }
+//   }
+
+//   return oddNumbers;
+// }
+
+// const result = getInitialOdds(6);
+// console.log(result);
+
+// 2)
+
+// function getEvenReverse(arr) {
+//   let evenNumbers = [];
+//   let k = arr.at(-1);
+
+//   for (i = 0; i <= k; i++) {
+//     if (arr[i] % 2 == 0) {
+//       evenNumbers.push(arr[i]);
+//     }
+//   }
+
+//   return evenNumbers;
+// }
+
+// const result = [2, 3, 6, 7];
+// console.log(getEvenReverse(result));
+
+// 3)
+
+
